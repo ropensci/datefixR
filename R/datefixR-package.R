254829f8ed2ab28183c9bfb29e8653ef1713feca
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2022-06-06 21:47:02 +0100

    Update package help doc

--- R/datefixR-package.R
+++ R/datefixR-package.R
@@ -1,10 +1,9 @@
 #' datefixR: Fix Really Messy Dates
 #'
-#' @section Description:
-#' Fixes messy dates in data frames such as those entered via text boxes.
-#' Standardizes / - and whitespace separation, month abbreviations, and year
-#' first or day first by converting to R's built-in Date class. Imputes missing
-#' date or month using user-provided values.
+#' @description Fixes messy dates in data frames such as those entered via text
+#' boxes. Standardizes / - and whitespace separation, month abbreviations, and
+#' year first or day first by converting to R's built-in Date class. Imputes
+#' missing date or month using user-provided values.
 #'
 #' @section Author:
 #' \strong{Maintainer}: Nathan Constantine-Cooke
@@ -14,7 +13,7 @@
 #' @seealso
 #' \url{https://www.constantine-cooke.com/datefixR/}
 #'
-#' \url{https://www.github.com/nathansam/datefixR/}
+#' \url{https://github.com/nathansam/datefixR/}
 ## usethis namespace: start
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
