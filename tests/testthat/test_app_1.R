bf90b9e4d7a969664dc66d69b91a17dd11f17924
Daniel Possenriede <possenriede@gmail.com>
2022-09-26 17:37:51 +0200

    skip if not macOS

--- tests/testthat/test_app_1.R
+++ tests/testthat/test_app_1.R
@@ -3,11 +3,8 @@
 test_that("fix_date_app() works", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
+  skip_if_not(Sys.info()["sysname"] == "Darwin", message = "Not run on macOS")
   withr::local_package("shinytest")
 
-  if (Sys.info()["sysname"] == "Darwin") {
-    expect_pass(testApp(test_path("apps/fix_date_theme/"),
-      compareImages = TRUE
-    ))
-  }
+  expect_pass(testApp(test_path("apps/fix_date_theme/"), compareImages = TRUE))
 })
