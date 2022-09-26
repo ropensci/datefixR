9da9b9d9d07da140f0d74d903995ca4620956d25
Daniel Possenriede <possenriede@gmail.com>
2022-09-26 13:24:03 +0200

    load shinytest only locally

--- tests/testthat/test_app_1.R
+++ tests/testthat/test_app_1.R
@@ -1,10 +1,9 @@
 # This file is for testing the applications in the apps/ directory.
 
-library(shinytest)
-
 test_that("fix_date_app() works", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
+  withr::local_package("shinytest")
 
   if (Sys.info()["sysname"] == "Darwin") {
     expect_pass(testApp(test_path("apps/fix_date_theme/"),
