27052d8d7253ac6731eb9f258e272cc5c948c3d7
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2023-01-16 17:03:34 +0000

    Update test_app_1.R

--- tests/testthat/test_app_1.R
+++ tests/testthat/test_app_1.R
@@ -6,10 +6,7 @@ test_that("fix_date_app() works with datefixR theme", {
   skip_if_not(Sys.info()["sysname"] == "Darwin", message = "Not run on macOS")
   withr::local_package("shinytest2")
   shiny_app <- fix_date_app()
-
   app <- AppDriver$new(shiny_app, variant = platform_variant())
-
-
   app$set_window_size(width = 779, height = 853)
   # Uploaded file outside of: ./tests/testthat
   app$upload_file(datafile = system.file("example.xlsx", package = "datefixR"))
@@ -22,12 +19,12 @@ test_that("fix_date_app() works with datefixR theme", {
   app$click("do")
   # Update output value
   # Update unbound `input` value
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 1)
   app$set_inputs(`selected.columns` = "Apple")
   app$set_inputs(`selected.columns` = c("Apple", "Pear"))
   app$click("do")
   # Update output value
-  app$expect_screenshot()
+  app$expect_screenshot(threshold = 1)
 })
 
 test_that("fix_date_app() works with shiny theme", {
@@ -36,10 +33,8 @@ test_that("fix_date_app() works with shiny theme", {
   skip_if_not(Sys.info()["sysname"] == "Darwin", message = "Not run on macOS")
   withr::local_package("shinytest2")
   shiny_app <- fix_date_app(theme = "none")
-
+  
   app2 <- AppDriver$new(shiny_app, variant = "shiny")
-
-
   app2$set_window_size(width = 779, height = 853)
   # Uploaded file outside of: ./tests/testthat
   app2$upload_file(datafile = system.file("example.csv", package = "datefixR"))
@@ -52,10 +47,10 @@ test_that("fix_date_app() works with shiny theme", {
   app2$click("do")
   # Update output value
   # Update unbound `input` value
-  app2$expect_screenshot()
+  app2$expect_screenshot(threshold = 1)
   app2$set_inputs(`selected.columns` = "Apple")
   app2$set_inputs(`selected.columns` = c("Apple", "Pear"))
   app2$click("do")
   # Update output value
-  app2$expect_screenshot()
+  app2$expect_screenshot(threshold = 1)
 })
