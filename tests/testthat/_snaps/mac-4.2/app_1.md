9991947453654e907dd85e42c4974172190ad5ad
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2023-01-17 11:17:32 +0000

    Expect inputs instead of snapshots

---
+++ tests/testthat/_snaps/mac-4.2/app_1.md
@@ -0,0 +1,4 @@
+# fix_date_app() works with datefixR theme
+
+    "fixed.csv"
+
--- tests/testthat/test_app_1.R
+++ tests/testthat/test_app_1.R
@@ -12,19 +12,20 @@ test_that("fix_date_app() works with datefixR theme", {
   app$upload_file(datafile = system.file("example.xlsx", package = "datefixR"))
   app$click("do")
   # Update output value
-  # Update unbound `input` value
-  app$set_inputs(`selected.columns` = "Apple")
-  app$set_inputs(`selected.columns` = c("Apple", "Pear"))
-  app$set_inputs(`selected.columns` = c("Apple", "Pear", "Lemon"))
-  app$click("do")
-  # Update output value
-  # Update unbound `input` value
-  app$expect_screenshot(threshold = 1)
+  app$set_inputs(df_rows_current = c(1, 2, 3, 4), allow_no_input_binding_ = TRUE, wait_ = FALSE)
+  app$set_inputs(df_rows_all = c(1, 2, 3, 4), allow_no_input_binding_ = TRUE, wait_ = FALSE)
+  app$set_inputs(df_state = c(1673953581559, 0, 10, "", TRUE, FALSE, TRUE, c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE)), allow_no_input_binding_ = TRUE, wait_ = FALSE)
   app$set_inputs(`selected.columns` = "Apple")
   app$set_inputs(`selected.columns` = c("Apple", "Pear"))
   app$click("do")
+  app$expect_values()
   # Update output value
-  app$expect_screenshot(threshold = 1)
+  app$set_inputs(df_rows_current = c(1, 2, 3, 4), allow_no_input_binding_ = TRUE, wait_ = FALSE)
+  app$set_inputs(df_rows_all = c(1, 2, 3, 4), allow_no_input_binding_ = TRUE, wait_ = FALSE)
+  app$set_inputs(df_state = c(1673953593973, 0, 10, "", TRUE, FALSE, TRUE, c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE)), allow_no_input_binding_ = TRUE)
+  app$expect_download("downloadData")
+  app$set_window_size(width = 779, height = 796)
+  app$expect_values()
 })
 
 test_that("fix_date_app() works with shiny theme", {
@@ -33,24 +34,24 @@ test_that("fix_date_app() works with shiny theme", {
   skip_if_not(Sys.info()["sysname"] == "Darwin", message = "Not run on macOS")
   withr::local_package("shinytest2")
   shiny_app <- fix_date_app(theme = "none")
-
   app2 <- AppDriver$new(shiny_app, variant = "shiny")
   app2$set_window_size(width = 779, height = 853)
   # Uploaded file outside of: ./tests/testthat
   app2$upload_file(datafile = system.file("example.csv", package = "datefixR"))
   app2$click("do")
   # Update output value
-  # Update unbound `input` value
-  app2$set_inputs(`selected.columns` = "Apple")
-  app2$set_inputs(`selected.columns` = c("Apple", "Pear"))
-  app2$set_inputs(`selected.columns` = c("Apple", "Pear", "Lemon"))
-  app2$click("do")
-  # Update output value
-  # Update unbound `input` value
-  app2$expect_screenshot(threshold = 1)
+  app2$set_inputs(df_rows_current = c(1, 2, 3, 4), allow_no_input_binding_ = TRUE, wait_ = FALSE)
+  app2$set_inputs(df_rows_all = c(1, 2, 3, 4), allow_no_input_binding_ = TRUE, wait_ = FALSE)
+  app2$set_inputs(df_state = c(1673953581559, 0, 10, "", TRUE, FALSE, TRUE, c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE)), allow_no_input_binding_ = TRUE, wait_ = FALSE)
   app2$set_inputs(`selected.columns` = "Apple")
   app2$set_inputs(`selected.columns` = c("Apple", "Pear"))
   app2$click("do")
+  app2$expect_values()
   # Update output value
-  app2$expect_screenshot(threshold = 1)
+  app2$set_inputs(df_rows_current = c(1, 2, 3, 4), allow_no_input_binding_ = TRUE, wait_ = FALSE)
+  app2$set_inputs(df_rows_all = c(1, 2, 3, 4), allow_no_input_binding_ = TRUE, wait_ = FALSE)
+  app2$set_inputs(df_state = c(1673953593973, 0, 10, "", TRUE, FALSE, TRUE, c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE), c(TRUE, "", TRUE, FALSE, TRUE)), allow_no_input_binding_ = TRUE)
+  app2$expect_download("downloadData")
+  app2$set_window_size(width = 779, height = 796)
+  app2$expect_values()
 })
