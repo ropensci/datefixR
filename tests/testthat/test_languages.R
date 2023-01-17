c0c175935188d08b1ba0f86181145bd29e197be7
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2023-01-17 14:01:06 +0000

    Only test single-byte char warning on MacOS

--- tests/testthat/test_languages.R
+++ tests/testthat/test_languages.R
@@ -280,12 +280,14 @@ if (l10n_info()$MBCS) {
   })
 }
 
-test_that("Warning if multibyte support is not detected", {
-  withr::with_locale(new = c("LC_CTYPE" = "en_US.US-ASCII"), code = {
-    expect_warning(.accept_multi_byte(), paste0(
-      "The current locale does not support multibyte characters. ",
-      "You may run into difficulties if any months are given as ",
-      "non-English language names. \n"
-    ))
+if (Sys.info()[["sysname"]] == "Darwin") {
+  test_that("Warning if multibyte support is not detected", {
+    withr::with_locale(new = c("LC_CTYPE" = "en_US.US-ASCII"), code = {
+      expect_warning(.accept_multi_byte(), paste0(
+        "The current locale does not support multibyte characters. ",
+        "You may run into difficulties if any months are given as ",
+        "non-English language names. \n"
+      ))
+    })
   })
-})
+}
