b8aa72978a75a04dd301dc819e268d6e20febcb4
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2025-07-29 15:00:10 +0100

    Update translate.h

--- src/translate.h
+++ src/translate.h
@@ -1,9 +1,8 @@
 #include <R.h>  /* to include Rconfig.h */
 
-#ifdef ENABLE_NLS
+#if defined(ENABLE_NLS) && !defined(_WIN32)
 #include "libintl.h"
 #define _(String) dgettext ("datefixR", String)
-/* replace pkg as appropriate */
 #else
 #define _(String) (String)
 #endif
