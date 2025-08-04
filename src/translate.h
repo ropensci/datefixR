c46bd94e2e55238fec3764d3fb284d3c148269af
Nathan Constantine-Cooke <nathan.constantine-cooke@ed.ac.uk>
2025-08-04 14:44:18 +0100

    Remove unused files

--- src/translate.h
+++
@@ -1,8 +0,0 @@
-#include <R.h>  /* to include Rconfig.h */
-
-#if defined(ENABLE_NLS) && !defined(_WIN32)
-#include "libintl.h"
-#define _(String) dgettext ("datefixR", String)
-#else
-#define _(String) (String)
-#endif
